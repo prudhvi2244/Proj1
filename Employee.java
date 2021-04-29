49426f4be62b214e1bda53bc967f87d6b63d7853
prudhvi4399 <prudhvi88801@gmail.com>
2021-04-29 16:26:16 +0530

    Added one method for displaying All Employees displayEmployees

--- Employee.java
+++ Employee.java
@@ -1,5 +1,9 @@
 class Employee
 {
+	public void displayEmployees()
+	{
+		System.out.println("Welcome to Display Employees");
+	}
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to main() method");
